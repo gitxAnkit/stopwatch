27205c4ecebd86a35ab1424a693e0d7c45af983d
Ankit Verma <ankitv9936@gmail.com>
2023-09-14 18:43:08 +0530

    Done

---
+++ script.js
@@ -0,0 +1,34 @@
+const clock = document.querySelector("#clock");
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+
+let sec =0,
+min=0,
+hr=0,
+intervalId;
+
+const startStopwatch =() =>{
+    intervalId = setInterval(()=>{
+        if(sec < 59){
+            sec++;
+        }
+        else if(min >= 59){
+            hr++;
+            min =0;
+        }
+        else{
+            sec=0;
+            min++;
+        }
+        const seconds =String(sec).padStart(2,"0");
+        const minutes =String(min).padStart(2,"0");
+        const hours =String(hr).padStart(2,"0");
+
+        clock.innerText = `${hours}:${minutes}:${seconds}`;
+        console.log(`${hours}:${minutes}:${seconds}`);
+    },1000)
+};
+
+const stopStopwatch =()=>{
+    clearInterval(intervalId);
+};
